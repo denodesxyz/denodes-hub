843d6df0c8c27842fcd42cf7103788edccef5529
deNodes <139079136+denodesxyz@users.noreply.github.com>
2023-09-08 15:39:10 +0200

    Updated to the latest version

--- scripts/subspace.sh
+++ scripts/subspace.sh
@@ -22,10 +22,10 @@ sudo apt -qq update
 sudo apt -qq -y upgrade
 sudo apt -y install wget
 
-BIN_FILE="https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.5-alpha"
+BIN_FILE="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.6-alpha"
 
 if [[ $1 == "v2" ]]; then
-    BIN_FILE="https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-v2-v0.6.5-alpha"
+    BIN_FILE="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-v2-v0.6.6-alpha"
 fi
 sudo wget -O /usr/local/bin/subspace $BIN_FILE
 sudo chmod +x /usr/local/bin/subspace
